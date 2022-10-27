efa4f0f3f5f3c1a65fd0720a7e68353ff786e27b
i-tanaka730 <pttp730@gmail.com>
2022-10-27 22:17:42 +0900

    feat: Paginationの実装誤りを修正。

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,9 +1,8 @@
-
 import Link from 'next/link';
 
 const Pagination = ({ pages, current_page = 1 }) => {
   return (
-    <div class="flex items-center space-x-1 mt-8">
+    <div className="flex items-center space-x-1 mt-8">
       {pages.map((page) => (
         <Link href={`/page/${page}`} key={page}>
           <a
