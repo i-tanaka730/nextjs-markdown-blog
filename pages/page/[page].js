864eadc35e2b6f97e299c6fb52d72cadfd9bb0b4
i-tanaka730 <pttp730@gmail.com>
2022-10-27 21:55:14 +0900

    feat: ページネーションの実装。

--- pages/page/[page].js
+++ pages/page/[page].js
@@ -1,60 +1,71 @@
 import fs from 'fs';
 import matter from 'gray-matter';
-import Pagination from '../components/Pagination';
-import PostCard from '../components/PostCard';
-
+import Pagination from '../../components/Pagination';
+import PostCard from '../../components/PostCard';
 const PAGE_SIZE = 5;
 
 const range = (start, end, length = end - start + 1) =>
   Array.from({ length }, (_, i) => start + i);
 
-/**
- * ビルド時に実行される。
- */
-export const getStaticProps = () => {
-  // postsフォルダからファイル一覧を取得
+export async function getStaticPaths() {
+  const files = fs.readdirSync('posts');
+  const count = files.length;
+
+  const paths = range(1, Math.ceil(count / PAGE_SIZE)).map((i) => ({
+    params: { page: i.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const current_page = params.page;
   const files = fs.readdirSync('posts');
   const posts = files.map((fileName) => {
-    // ファイルから「.md」の拡張子を除去した文字列をスラッグとする
     const slug = fileName.replace(/\.md$/, '');
-    // ファイルの内容を読み込み
     const fileContent = fs.readFileSync(`posts/${fileName}`, 'utf-8');
-    // ファイルのメタ情報を取得
     const { data } = matter(fileContent);
+
     return {
       frontMatter: data,
       slug,
     };
   });
 
-  // 日付の降順にソート
+  const pages = range(1, Math.ceil(posts.length / PAGE_SIZE));
+
   const sortedPosts = posts.sort((postA, postB) =>
     new Date(postA.frontMatter.date) > new Date(postB.frontMatter.date) ? -1 : 1
   );
 
-  const pages = range(1, Math.ceil(posts.length / PAGE_SIZE));
+  const slicedPosts = sortedPosts.slice(
+    PAGE_SIZE * (current_page - 1),
+    PAGE_SIZE * current_page
+  );
 
   return {
     props: {
-      posts: sortedPosts.slice(0, PAGE_SIZE),
+      posts: slicedPosts,
       pages,
+      current_page,
     },
   };
-};
+}
 
-/**
- * デフォルト処理。
- */
-export default function Home({ posts, pages }) {
+const Page = ({ posts, pages, current_page }) => {
   return (
     <div className="my-8">
       <div className="grid grid-cols-3 gap-4">
         {posts.map((post) => (
-          // getStaticPropsで設定したスラッグ・メタ情報をPostCardへ受け渡し
           <PostCard key={post.slug} post={post} />
         ))}
       </div>
-      <Pagination pages={pages} />
+      <Pagination pages={pages} current_page={current_page} />
     </div>
   );
-}
+};
+
+export default Page;
